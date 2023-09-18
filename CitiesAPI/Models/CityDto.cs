1799d09a3252f354cc0b72ad1668aefd79bac86a
rlauries <rlauries001@gmail.com>
2023-09-18 11:40:34 -0400

    Create Model classes

---
+++ CitiesAPI/Models/CityDto.cs
@@ -0,0 +1,14 @@
+﻿namespace CitiesAPI.Models
+{
+    public class CityDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+
+       public List<PointOfInterestDto> PointOfInterests { get; set;}
+
+
+
+    }
+}
---
+++ CitiesAPI/Models/PointOfInterestDto.cs
@@ -0,0 +1,14 @@
+﻿namespace CitiesAPI.Models
+{
+    public class PointOfInterestDto
+    {
+        
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string? Description { get; set; }
+
+
+
+        
+    }
+}
