3639f421ebf460045be516448ce5c0ff5fb7e6d3
rlauries <rlauries001@gmail.com>
2023-09-18 11:53:27 -0400

    Create Data Store for cities

---
+++ CitiesAPI/CitiesDataStore.cs
@@ -0,0 +1,66 @@
+﻿using CitiesAPI.Models;
+
+namespace CitiesAPI
+{
+    public class CitiesDataStore
+    {
+
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with a big park.",
+                    PointOfInterests = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "The most visited urban Park in US"
+
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Empire State Building",
+                            Description = "A 102-story skyscraper located in Midtown Manhattan"
+                        }
+                    }
+
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "The one with a cathedral.",
+                    PointOfInterests  = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto
+                        {
+                            Id = 1,
+                            Name = "Aberowen",
+                            Description = "The most visited church"
+                        }
+                    }
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Paris",
+                    Description = "The one with a big tower."
+
+                }
+
+            };
+        }
+
+
+
+    }
+}
